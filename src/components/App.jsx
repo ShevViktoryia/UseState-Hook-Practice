c602fc925c56bc0c3b9b0231a9f1b373d4947b1b
Victoria <shevvictoryia@gmail.com>
2024-11-01 00:24:09 +0300

    feat: add update time by interval

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 
 function App() {
+  setInterval(updateTime, 1000);
   let time = new Date().toLocaleTimeString();
   const [state, setTime] = useState(time);
 
   function updateTime() {
-    const curTime = (time = new Date().toLocaleTimeString());
+    const curTime = new Date().toLocaleTimeString();
     setTime(curTime);
   }
   return (
     <div className="container">
-      <h1>{time}</h1>
+      <h1>{state}</h1>
       <button onClick={updateTime}>Get Time</button>
     </div>
   );
