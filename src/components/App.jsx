b3ca2416d40ad6c2949f06897886453e02290241
Victoria <shevvictoryia@gmail.com>
2024-11-01 00:18:17 +0300

    feat: add update time by btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  const [state, setTime] = useState(time);
+
+  function updateTime() {
+    const curTime = (time = new Date().toLocaleTimeString());
+    setTime(curTime);
+  }
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
